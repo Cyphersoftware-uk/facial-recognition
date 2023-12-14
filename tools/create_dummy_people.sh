46465a42d18b09b4284a4c73acade97ff2fe6b80
jake0001-dev <root@node2.cyphersoftware.uk>
2023-12-14 14:12:19 +0000

    modified:   tools/create_dummy_people.sh

--- tools/create_dummy_people.sh
+++ tools/create_dummy_people.sh
@@ -1,9 +1,9 @@
 URL1="http://45.87.28.51:5000/register?ID=1&Name=rishi%20sunak"
-URL2="http://45.87.28.51:5000/register?ID=2&Name=boriz%20johnson"
-URL3="http://45.87.28.51:5000/register?ID=3&Name=abdullah%20example"
-URL4="http://45.87.28.51:5000/register?ID=4&Name=jazza%20dave"
+URL2="http://45.87.28.51:5000/register?ID=2&Name=Boris%20johnson"
+URL3="http://45.87.28.51:5000/register?ID=3&Name=abdullah%20Albaz"
+URL4="http://45.87.28.51:5000/register?ID=4&Name=Jezza%20Clarkson"
 URL5="http://45.87.28.51:5000/register?ID=5&Name=richard%20hammond"
-URL6="http://45.87.28.51:5000/register?ID=6&Name=rishi%20dolittle"
+URL6="http://45.87.28.51:5000/register?ID=6&Name=James%20May"
 URL7="http://45.87.28.51:5000/register?ID=7&Name=Harvey%20Wimpenny"
 URL8="http://45.87.28.51:5000/register?ID=8&Name=Marcus%20Rashford"
 
